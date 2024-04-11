dcaa587ec87b166d38e62b2d653b96c56467d27d
Patrick Barker <patrickbarkerco@gmail.com>
2024-04-11 12:00:12 -0600

    models

---
+++ devicebay/db/models.py
@@ -0,0 +1,41 @@
+import uuid
+import time
+
+from sqlalchemy import Column, String, Boolean, Float, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class UserRecord(Base):
+    __tablename__ = "users"
+
+    email = Column(String, unique=True, index=True, primary_key=True)
+    display_name = Column(String)
+    handle = Column(String)
+    picture = Column(String)
+    created = Column(Integer)
+    updated = Column(Integer)
+
+
+class DeviceTypeRecord(Base):
+    __tablename__ = "device_types"
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+    image = Column(String)
+    versions = Column(String)
+    env_opts = Column(String)
+    supported_runtimes = Column(String)
+    owner_id = Column(String)
+    public = Column(Boolean)
+    icon = Column(String)
+    created = Column(Float, default=time.time)
+    updated = Column(Float, default=time.time)
+    mem_request = Column(String)
+    mem_limit = Column(String)
+    cpu_request = Column(String)
+    cpu_limit = Column(String)
+    gpu_mem = Column(String)
+    llm_providers = Column(String, nullable=True)
--- devicebay/models.py
+++ devicebay/models.py
@@ -1,6 +1,45 @@
+from typing import Optional, Dict, List
 from pydantic import BaseModel
 
 
 class DeviceModel(BaseModel):
     name: str
     config: BaseModel
+
+
+class EnvVarOptModel(BaseModel):
+    name: str
+    description: Optional[str] = None
+    required: bool = False
+    default: Optional[str] = None
+    secret: bool = False
+    options: List[str] = []
+
+
+class LLMProviders(BaseModel):
+    preference: List[str] = []
+
+
+class DeviceTypeModel(BaseModel):
+    id: Optional[str] = None
+    name: str
+    owner_id: Optional[str] = None
+    description: str
+    image: Optional[str] = None
+    versions: Optional[Dict[str, str]] = None
+    env_opts: List[EnvVarOptModel] = []
+    supported_runtimes: List[str] = []
+    created: Optional[float] = None
+    updated: Optional[float] = None
+    public: bool = False
+    icon: Optional[str] = None
+    mem_request: Optional[str] = "500m"
+    mem_limit: Optional[str] = "2gi"
+    cpu_request: Optional[str] = "1"
+    cpu_limit: Optional[str] = "4"
+    gpu_mem: Optional[str] = None
+    llm_providers: Optional[LLMProviders] = None
+
+
+class DeviceTypesModel(BaseModel):
+    types: List[DeviceTypeModel]
